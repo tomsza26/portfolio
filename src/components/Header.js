d5f9011e90e9348e986c367045ec6c27aabc11cc
tomsza26 <tomsza27@gmail.com>
2020-02-03 21:17:55 +0100

    wykonano wstepny komponent main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import '../App.scss';
+import Particles from 'react-particles-js';
+import particles from './particlesjs.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const particleOpt = particles;
 
 class Header extends React.Component {
 	render() {
 		return (
-			<React.Fragment>
-				<header>
-					<div id="headerIn">
-						<nav />
-						<div id="headerContent">
-							<div id="leftContainer">
-								<a href="https://github.com/tomsza26" target="_blank">
-									<FontAwesomeIcon icon={faGithub} size="3x" color="#f05133" />
-								</a>
-							</div>
-							<div id="nameContainer">
-								<div id="name">//Tomasz Szablewski</div>
-								<div id="frontWeb">Front End Web Developer </div>
-							</div>
+			<header>
+				<Particles params={particleOpt} />
+				<div id="headerIn">
+					<nav />
+					<div id="headerContent">
+						<div id="leftContainer">
+							<a href="https://github.com/tomsza26" target="_blank" rel="noopener noreferrer">
+								<FontAwesomeIcon icon={faGithub} size="3x" color="#f05133" />
+							</a>
+						</div>
+						<div id="nameContainer">
+							<div id="name">//Tomasz Szablewski</div>
+							<div id="frontWeb">Front End Web Developer </div>
 						</div>
 					</div>
-				</header>
-				<section />
-				<footer />
-			</React.Fragment>
+				</div>
+			</header>
 		);
 	}
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,60 @@
 import React from 'react';
 import '../App.scss';
-
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-	console.log('callback - particles.js config loaded');
-});
+import projects from './projects.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faReact, faJs, faGitAlt, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faDesktop, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 
 class Main extends React.Component {
 	render() {
 		return (
-			<React.Fragment>
-				<div id="particles-js" />
-			</React.Fragment>
+			<main>
+				<section id="iconContainer">
+					<FontAwesomeIcon icon={faHtml5} size="4x" color="#e34d26" />
+					<FontAwesomeIcon icon={faCss3Alt} size="4x" color="#0070bb" />
+					<FontAwesomeIcon icon={faReact} size="4x" color="#61d9fa" />
+					<FontAwesomeIcon icon={faJs} size="4x" color="#f7df1e" />
+					<FontAwesomeIcon icon={faGitAlt} size="4x" color="#f05133" />
+					<FontAwesomeIcon icon={faSass} size="4x" color="#cc6699" />
+				</section>
+				<article id="projects">
+					<header>Projects</header>
+					{projects.map((data, index) => {
+						return (
+							<section key={index}>
+								<div className="leftSide">
+									<header>{data.name}</header>
+									<img
+										src={require('../images' + data.image)}
+										alt="img"
+										width="300px"
+										height="200px"
+									/>
+									<div className="links">
+										<a href={data.linkLive} target="_blank" rel="noopener noreferrer">
+											<FontAwesomeIcon icon={faDesktop} size="1x" color="#f05133" /> Live preview
+										</a>
+										<a href={data.linkCode} target="_blank" rel="noopener noreferrer">
+											<FontAwesomeIcon icon={faCodeBranch} size="1x" color="#f05133" /> Source
+											code
+										</a>
+									</div>
+									<header>Technologies used</header>
+									<ul>
+										{data.techUsed.map((list, i) => {
+											return <li key={i}>{list}</li>;
+										})}
+									</ul>
+								</div>
+								<div className="rightSide">
+									<header>Overwiew</header>
+									<div className="overwiev">{data.overwiew}</div>
+								</div>
+							</section>
+						);
+					})}
+				</article>
+			</main>
 		);
 	}
 }
