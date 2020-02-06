55b98b2330d601b7b88cc66cfa37b9465cc83ce5
tomsza26 <tomsza27@gmail.com>
2020-02-06 22:08:54 +0100

    Updates

---
+++ precache-manifest.b366e5f9bab7162e385f2095df8b5e72.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5264ac580471da2c3a8c340f69a2162b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ff085a142d4e2ee04006",
+    "url": "/portfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "35fc61d9fb54d4b9b849",
+    "url": "/portfolio/static/css/main.cf5c301f.chunk.css"
+  },
+  {
+    "revision": "ff085a142d4e2ee04006",
+    "url": "/portfolio/static/js/2.f5d414bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f5d414bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35fc61d9fb54d4b9b849",
+    "url": "/portfolio/static/js/main.88e1121f.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "31f549b4a1627583895ee230df8d6645",
+    "url": "/portfolio/static/media/calculator.31f549b4.gif"
+  },
+  {
+    "revision": "24055dc0c99a863ac657cd0c6451b238",
+    "url": "/portfolio/static/media/drum.24055dc0.gif"
+  },
+  {
+    "revision": "23e2b6cf75076c68a788053631d1f14d",
+    "url": "/portfolio/static/media/ep_naturalblack.23e2b6cf.png"
+  },
+  {
+    "revision": "0b1d5485e75e772b353a22c18ed42910",
+    "url": "/portfolio/static/media/pomodoro.0b1d5485.gif"
+  },
+  {
+    "revision": "cc3fe51b0c16b0959a7ef6cf8561ebf5",
+    "url": "/portfolio/static/media/portfolio.cc3fe51b.png"
+  },
+  {
+    "revision": "646405d1f6e8b2c5e7fc9ce26fc91ef5",
+    "url": "/portfolio/static/media/quotes.646405d1.gif"
+  }
+]);
